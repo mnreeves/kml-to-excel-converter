0677769b2531355439951d54a540a15e5b7b7022
natsir-productions <natsir.productions@gmail.com>
2021-11-19 15:08:32 +0700

    update converter

--- convert.js
+++ convert.js
@@ -8,22 +8,38 @@ const run = async () => {
   const worksheet = workbook.addWorksheet("Sheet1");
 
   worksheet.columns = [
-    { header: 'No_Bid', key: 'noBid'},
-    { header: 'location', key: 'location'}
+    { header: 'name', key: 'name'},
+    { header: 'description', key: 'description'},
+    { header: 'coordinates', key: 'coordinates'},
+    { header: 'location', key: 'location'},
+    { header: 'type', key: 'type' }
   ];
 
   const features = jsonfile['features'];
-  for (let k=0; k<features.length; k++) {
-    const noBid = features[k]['properties']['No_Bid'];
-    const location = transformLocation(features[k]['geometry']);
+
+  for (let k = 0; k < features.length; k++) {
+    const name = features[k]['properties']['name'] || '';
+    const description = features[k]['properties']['description'] || '';
+    const type = features[k]['geometry']['type'] || '';
+
+    const wkt = transformToWkt(features[k]['geometry']);
+
     const data = {
-      noBid,
-      location
+      name,
+      description,
+      type
     };
+
+    if (type == 'Point' || type == 'LineString') {
+      data['coordinates'] = wkt;
+    } else if (type == 'Polygon') {
+      data['location'] = wkt;
+    }
+
     worksheet.addRow(data);
   }
   
-  await workbook.xlsx.writeFile('MDA_bidang2020_Full.xlsx');
+  await workbook.xlsx.writeFile('Master Site Layout.xlsx');
 
   console.log("File is written");
 };
@@ -31,26 +47,64 @@ const run = async () => {
 /**
  * @param {object} objLocation 
  */
-const transformLocation = (objLocation) => {
+const transformToWkt = (objLocation) => {
+  const data = {};
+  data['type'] = objLocation['type'];
+  
   if (objLocation['type'] === 'Polygon') {
-    const data = {};
-    data['type'] = objLocation['type'];
     data['coordinates'] = cleanCoordinatePoligonArray(objLocation['coordinates']);
-
-    const wktData = stringify(data);
-    return wktData;
   } else if (objLocation['type'] === 'GeometryCollection') {
-    const data = {};
-    data['type'] = objLocation['type'];
     data['geometries'] = cleanCoordinateGCArray(objLocation['geometries']);
-    
-    const wktData = stringify(data);
-    return wktData
+  } else if (objLocation['type'] === 'Point') {
+    data['coordinates'] = transformPointTo2D(objLocation['coordinates']);
+  } else if (objLocation['type'] === 'LineString') {
+    data['coordinates'] = transformLineStringTo2D(objLocation['coordinates']);
   } else {
     // todo: handle other type...
+    console.log('error bang...');
     throw new Error('other type not handle yet');
   }
+
+  const wktData = stringify(data);
+  // RAW DATA //
+  // NO NEED TO CLEANUP
+  // 1. Z STRING
+  // 2. 3D POINTS
+  // --------- //
+  // const data = {};
+  // data['type'] = objLocation['type'];
+  // data['coordinates'] = objLocation['coordinates'];
+  // const wktData = stringify(data);
+
+  return wktData;
 };
+
+
+/**
+ * @param {Array<Integer>} array - [1, 2, 3]
+ */
+const transformPointTo2D = (array) => {
+  if (array && array.length > 0) {
+    array.pop()
+    return array;
+  } else {
+    return [];
+  }
+}
+
+
+/**
+ * @param {Array<Array>} array - [ [1, 1, 1], [2, 2, 2], [3, 3, 3], ...]
+ */
+const transformLineStringTo2D = (array) => {
+  if (array && array.length > 0) {
+    array.forEach(arr => arr.pop());
+    return array;
+  } else {
+    return array;
+  }
+};
+
 
 /**
  * @param {Array} array 
